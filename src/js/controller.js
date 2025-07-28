1b2c1f814b21a5f8e9fdc8a1b925fc897e26e018
Shelley <jialing80916@gmail.com>
2025-07-28 16:49:49 +0800

    init file

---
+++ src/js/controller.js
@@ -0,0 +1,14 @@
+// import * as model from './model.js';
+// import { MODAL_CLOSE_SEC } from './config.js';
+
+// import "core-js/actual";
+// import "regenerator-runtime/runtime";
+
+// if (model.hot) {
+//   model.hot.accept();
+// }
+
+const init = function () {
+  console.log("Hello World!");
+};
+init();
