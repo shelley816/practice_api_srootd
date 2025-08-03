9865beed5cc0346432566f8efcc697ec384123dc
Shelley <jialing80916@gmail.com>
2025-08-03 17:18:41 +0800

    Modified author and saved css

--- src/js/views/imagesView.js
+++ src/js/views/imagesView.js
@@ -1,3 +1,4 @@
+import icon from "url:../../img/icons.svg"; // Parcel 2
 import View from "./View.js";
 
 import { KEY_WORDS } from "../config";
@@ -26,19 +27,28 @@ class ImagesView extends View {
           <img src="${first.urls.regular}" alt="${first.alt}" />
         </div>
         <div class="preview__info">
-          <h2 class="preview__title">${first.keyword}</h2>
-          <p class="preview__description">${
-            first.description ? first.description : first.alt
-          }</p>
-          <div class="author">
-            <a href="#" target="_blank">
-              <div class="author__img">
-                <img src="${first.user.profile_image}" alt="${
+          <div>
+            <h2 class="preview__title">${first.keyword}</h2>
+            <p class="preview__description">${
+              first.description ? first.description : first.alt
+            }</p>
+            <div class="author">
+              <a href="${first.user.link}" target="_blank">
+                <div class="author__img">
+                  <img src="${first.user.profileImage.small}" alt="${
       first.user.bio
     }" />
-              </div>
-              <h3 class="author__name">${first.user.name}</h3>
-            </a>
+                </div>
+                <h3 class="author__name">${first.user.name}</h3>
+              </a>
+            </div>
+          </div>
+          <div class="saved__icon">
+            <button class="nav__btn">
+              <svg class="nav__icon">
+                <use href="${icon}#icon-star"></use>
+              </svg>
+            </button>
           </div>
         </div>
       </div>
@@ -48,22 +58,22 @@ class ImagesView extends View {
       .slice(1)
       .map((img) => {
         return `
-        <div class="preview__item">
-          <div class="preview__fig">
-            <img src="${img.urls.small}" alt="${img.description}" />
-          </div>
-          <div class="preview__info">
-            <h2 class="preview__title">${img.keyword}</h2>
-            <div class="author">
-              <a href="#" target="_blank">
-                <div class="author__img">
-                  <img src="${img.user.profile_image}" alt="${img.user.bio}" />
-                </div>
-                <h3 class="author__name">${img.user.name}</h3>
-              </a>
+          <div class="preview__item">
+            <div class="preview__fig">
+              <img src="${img.urls.regular}" alt="${img.alt}" />
+            </div>
+            <div class="preview__info">
+              <h2 class="preview__title">${img.keyword}</h2>
+              <div class="author">
+                <a href="${img.user.link}" target="_blank">
+                  <div class="author__img">
+                    <img src="${img.user.profileImage.small}" alt="${img.user.bio}" />
+                  </div>
+                  <h3 class="author__name">${img.user.name}</h3>
+                </a>
+              </div>
             </div>
           </div>
-        </div>
       `;
       })
       .join("");
